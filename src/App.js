79f67ced752f38bf6966b7a73c12cb29b73f998b
Andy Truong <andytruongcs@gmail.com>
2023-03-08 19:49:21 -0500

    Upload entire project

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { createClient } from '@supabase/supabase-js'
+
+import {
+  Auth,
+} from '@supabase/auth-ui-react'
+
+import {
+  // Import predefined theme
+  ThemeSupa,
+} from '@supabase/auth-ui-shared'
+
+
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_ANON_KEY
+)
+
+
+function App() {
+
+  return (
+
+    <div>
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+
+      />
+    </div>
+
+
+  );
+}
+
+export default App;
